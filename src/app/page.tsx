93099e18e05379df0d6e769f664d69fab7b5e9a0
bakrifly <bakri.hmouda@gmail.com>
2023-09-28 23:31:31 -0400

    new home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,85 +9,13 @@ export default function Home() {
   const [values, setValues] = useState();
 
   return (
-    <>
-      <h1 className="mb-6">Build an ADDWEB item</h1>
-      <form action="">
-        <div className="flex flex-col gap-4">
-          {/* active status of product */}
-          <div className="form-control">
-            <label className="label cursor-pointer justify-normal gap-4">
-              <span className="label-text">Active</span>
-              <input
-                type="checkbox"
-                className="toggle toggle-primary"
-                checked
-              />
-            </label>
-          </div>
-
-          <div className="flex justify-between">
-            {/* product reference number*/}
-            <input
-              className="input input-bordered input-primary"
-              placeholder="Items Reference#"
-              type="text"
-              required
-            />
-
-            {/* product price */}
-            <input
-              type="number"
-              className="input input-bordered input-primary"
-              placeholder="price tax excluded"
-            />
-          </div>
-
-          {/* Product Name/title */}
-          <input
-            type="text"
-            placeholder="Name"
-            className="input input-bordered input-primary w-full"
-          />
-
-          {/* product Descriptions */}
-          <textarea
-            className="textarea textarea-primary"
-            placeholder="Descriptions"
-          ></textarea>
-
-          {/* number of images. calculated based on REF# */}
-          <input
-            type="number"
-            className="input input-bordered input-primary"
-            placeholder="number of images"
-          />
-
-          {/* product categories */}
-          <Select
-            options={categories}
-            isMulti
-            placeholder={"Select Categories"}
-          />
-
-          {/* product supplier */}
-          <Select
-            options={suppliers}
-            isClearable
-            placeholder={"Select Supplier"}
-          />
-
-          {/* Select tax rule */}
-          <Select
-            options={[
-              { label: "books", value: 2 },
-              { label: "others", value: 1 },
-            ]}
-            isClearable
-            placeholder={"select tax rule"}
-          />
-        </div>
-      </form>
-      <pre>{JSON.stringify(values)}</pre>
-    </>
+    <div className="h-[400px] flex justify-center items-center">
+      <div>
+        <h1 className="text-7xl font-bold">Welcome to the Generator</h1>
+        <p className="text-center text-gray-400">
+          Please click one of the button above to get started.
+        </p>
+      </div>
+    </div>
   );
 }
